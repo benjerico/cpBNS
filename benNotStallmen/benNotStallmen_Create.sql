60ba46ebd39c9e2493b9576ca22dcb228d598c32
Ben15 <benjerico@gmail.com>
2015-12-03 08:34:22 -0600

    updated creation script to fix the productNum

--- benNotStallmen/benNotStallmen_Create.sql
+++ benNotStallmen/benNotStallmen_Create.sql
@@ -38,7 +38,7 @@ add constraint FK_customerID
 
 create table Product
 (
-	productID int Identity(1, 100000) primary key not null,
+	productID int Identity(100000, 1) primary key not null,
 	productNum nvarchar(20) not null,
 	price money not null,
 	prodWeight decimal(8,3) not null,
@@ -49,7 +49,8 @@ create table Product
 	productDesc nvarchar(400) null,
 	productPhotoThumb varbinary(max) null,
 	productPhotoLarge varbinary(max) null,
-	productPhotoFileName nvarchar(50) null
+	productPhotoFileName nvarchar(50) null,
+	productName nvarchar(100)
 );
 
 create table Sales
@@ -145,4 +146,13 @@ create nonclustered index idk_Product_productNumPriceColorProductPhotoThumb
 	, price ASC
 	, color
 );
+insert into Product values('CUP001234', 15.00, 3.00, 'WHITE', 'A001', 5, 50, 'A nice 12oz mug to hold only your choicest beverages.', NULL, NULL, NULL, 'Coffee Cup')
+;
+insert into Product values('MAT001234', 8.00, 1.12, 'ORANGE', 'A002', 3, 50, 'All your cubical farm friends will rave when they see this beauty, your very own Ben Not Stallmen Mouse pad. Made of organic neoprene this will surely be a delight to your office toils.', NULL, NULL, NULL, 'Mouse Pad')
+;
+insert into Product values('CUP005678', 13.55, 2.00, 'BLACK', 'A001', 3, 50, 'Be organized is Bens motto, well maybe. What ever it is that is his guruish montra you will certainly know where to stick your pens and pencils. If you do not just ask he will tell you. This baby talks to you. So when your board forget Stallman ask Ben.', NULL, NULL, NULL, 'Pencil Cup')
+;
+insert into Product values('TSHPEN1515', 25.00, 2.00, 'ORANGE', 'A003', 1, 50, 'Get this wonderfully comfortable, 100% inorganic cotton t-shirt with Ben, not Stallmans, fantastic mug on it. We only sell large shirts because that is what Ben wears and why would you want to wear anything else?', NULL, NULL, NULL, 'Shirt')
+;
+insert into Product values('BEN151234', 10.00, 1.25, 'BLUE', 'A001', 8, 50, 'Do you like Beer? Do you like cider? Well Ben does. So buy these pint glasses and enjoy that brew that you do. Heck if you play your cards right maybe you someday can drink one with Stallman, he could always use a friend.', NULL, NULL, NULL, 'Pint Glass')
 
